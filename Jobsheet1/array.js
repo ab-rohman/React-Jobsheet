946edebaacc5b998b20dadfa27db6abf33b1f2db
Abdul_Rohman <rokhman12abdul@gmail.com>
2022-02-17 21:40:35 +0700

    step h

---
+++ Jobsheet1/array.js
@@ -0,0 +1,7 @@
+//let [friends] = ['Ratna', 'Bunga','Tiara'];
+//declaring the first value of array into friends variable
+
+//modify the code
+let [, ,friends] = ['Ratna','Bunga','Tiara'];
+alert(friends);
+//show the value of the friends
