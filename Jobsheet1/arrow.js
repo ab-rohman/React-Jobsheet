150df4dc21bd8f309923b368a82981ffee87151c
Abdul_Rohman <rokhman12abdul@gmail.com>
2022-02-17 15:34:22 +0700

    step e

---
+++ Jobsheet1/arrow.js
@@ -0,0 +1,11 @@
+//without arrow
+//function greeting(message){ return alert(`${message} Girl!`);}
+
+//with arrow
+let greeting = message => alert(`${message} Girl!`);
+
+//pada jobsheet ini kita menggunakan cara pendeklarasian function dengan arrow
+
+// let greeting1 = function (message){ return alert(`${message} Girl!`);}
+//greeting1 sama saja hasilnya dengan function greeting
+greeting('Good Morning, ');
