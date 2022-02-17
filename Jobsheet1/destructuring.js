4e1d6184672667352e328308cd8acd02fb42fd98
Abdul_Rohman <rokhman12abdul@gmail.com>
2022-02-17 21:40:22 +0700

    step g

---
+++ Jobsheet1/destructuring.js
@@ -0,0 +1,13 @@
+let student = ({name, university}) => {
+    //declaring object student that has 2 attribute, name and unversity
+    alert(`${name} from ${university}`);
+    //show the value of every attribute
+}
+
+student({
+    //instantiated the object and give both of the attribute value
+    name: 'Abdul',
+    //abdul as value of name
+    university: 'State Polytechnic of Malang'
+    //'State ...' as value of unversity
+});
